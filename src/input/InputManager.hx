2b08014707f5b16ea6a5bed657be0a8643afa06f
Eiyeron Fulmincendii <eiyeron@gmail.com>
2014-11-06 21:32:03 +0100

    Once again I forgot the file.

---
+++ src/input/InputManager.hx
@@ -0,0 +1,47 @@
+package input;
+
+import luxe.Input.Key;
+
+
+/**
+ * This class serves as an input wrapper. It'll use keybindings to avoid stupid
+ * things like AZERTY/QWERTY conflict.
+ *
+ * TODO : Manage player actions
+ * TODO : I/O from/to a config file
+ * TODO : Live remapping
+ * TODO~: Multiple players
+ * TODO~: use get macros or a list based system to avoid too much code.
+ */
+class InputManager {
+	static var _instance: InputManager;
+
+	function new() {
+		// I/O file here
+	}
+
+	public static function instance(): InputManager {
+		if( _instance == null) _instance = new InputManager();
+		return _instance;
+	} 
+
+	public function playerUp(): Bool {
+		return Luxe.input.keydown(Key.key_z) || Luxe.input.keydown(Key.key_w);
+	}
+
+	public function playerDown(): Bool {
+		return Luxe.input.keydown(Key.key_s);
+	}
+
+	public function playerLeft(): Bool {
+		return Luxe.input.keydown(Key.key_a) || Luxe.input.keydown(Key.key_q);
+	}
+
+	public function playerRight(): Bool {
+		return Luxe.input.keydown(Key.key_d);
+	}
+
+	public function playerAction(): Bool {
+		return Luxe.input.keydown(Key.space);
+	}
+}
