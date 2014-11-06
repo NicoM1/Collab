14d81a23f311ec1308773256e8679593a497c1b5
Eiyeron Fulmincendii <eiyeron@gmail.com>
2014-11-06 21:13:54 +0100

    Made RenderMaths use Config.perspective

--- src/utils/RenderMaths.hx
+++ src/utils/RenderMaths.hx
@@ -6,7 +6,7 @@ class RenderMaths {
 	// TODO : Define the correct perspective transform. I totally used random values
 	public static inline function perspectiveProjection(position: Vector, renderPos: Vector): Vector {
 		 renderPos.x = position.x;
-		 var y = position.y / 1.75;
+		 var y = position.y / Config.perspective;
 		 renderPos.y = y + (position.y - y);
 		 return renderPos;
 	}
