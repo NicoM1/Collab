513ca7265a02f4692bfb0ec105e521256a557538
NicoM1 <nico.may99@gmail.com>
2014-11-05 17:05:37 -0800

    awesome art skillz

--- src/utils/RenderMaths.hx
+++ src/utils/RenderMaths.hx
@@ -5,6 +5,6 @@ import luxe.Vector;
 class RenderMaths {
 	// TODO : Define the correct perspective transform. I totally used random values
 	public static inline function perspectiveProjection(position: Vector) {
-		 return new Vector(position.x, position.y /4);
+		 return new Vector(position.x, position.y /3);
 	}
 }
