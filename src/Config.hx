6015bd738b9577772905f864384f02bb5ce906a3
Eiyeron Fulmincendii <eiyeron@gmail.com>
2014-11-06 22:45:56 +0100

    Implemented back LocalSAve with lot of changes to make it a small prettier. Still ugly as heck and need to store keys more clearly. :/

--- src/Config.hx
+++ src/Config.hx
@@ -1,7 +1,7 @@
 package;
 
 /**
- * ...
+ * NOTE Wait, isnt there the config.json for things like that?
  * @author 
  */
 
@@ -10,4 +10,13 @@ class Config{
 	public static var scale: Float = 1;
 	public static var perspective: Float = 1.75;
 	public static var horizon: Float = 0;
+	public static var inputConfigFilePath: String = "input.json"; // Yeah, JSON is human-editable.
+	public static var defaultKeyConfig: input.InputManager.InputKeyList = {
+		up: luxe.Input.Key.up,
+		down: luxe.Input.Key.down,
+		left: luxe.Input.Key.left,
+		right: luxe.Input.Key.right,
+		action: luxe.Input.Key.space,
+		exit: luxe.Input.Key.escape,
+	}
 }
--- src/input/InputManager.hx
+++ src/input/InputManager.hx
@@ -2,6 +2,16 @@ package input;
 
 import luxe.Input.Key;
 
+import io.LocalSave;
+
+typedef InputKeyList = {
+    ?up: Int,
+    ?down: Int,
+    ?left: Int,
+    ?right: Int,
+    ?action: Int,
+    ?exit: Int
+};
 
 /**
  * This class serves as an input wrapper. It'll use keybindings to avoid stupid
@@ -18,36 +28,54 @@ import luxe.Input.Key;
  class InputManager {
     static var _instance: InputManager;
 
+    var _localSave: LocalSave;
+    var _input: InputKeyList;
     function new() {
-        // I/O file here
+        _localSave = new LocalSave();
+        _input = _localSave.loadData("input");
+        
+        // Keybindings loading.
+        var mustSaveConfig: Bool = false;
+        // TODO : better way?
+        if( _input.up == null)      {mustSaveConfig = true; _input.up       = Config.defaultKeyConfig.up;}
+        if( _input.down == null)    {mustSaveConfig = true; _input.down     = Config.defaultKeyConfig.down;}
+        if( _input.left == null)    {mustSaveConfig = true; _input.left     = Config.defaultKeyConfig.left;}
+        if( _input.right == null)   {mustSaveConfig = true; _input.right    = Config.defaultKeyConfig.right;}
+        if( _input.action == null)  {mustSaveConfig = true; _input.action   = Config.defaultKeyConfig.action;}
+        if( _input.exit == null)    {mustSaveConfig = true; _input.exit     = Config.defaultKeyConfig.exit;}
+
+        if( mustSaveConfig)
+           _localSave.saveData(_input);
     }
 
     public static function instance(): InputManager {
-        if( _instance == null) _instance = new InputManager();
+        if( _instance == null) {
+            _instance = new InputManager();
+        }
         return _instance;
     } 
 
     public function playerUp(): Bool {
-        return Luxe.input.keydown(Key.key_z) || Luxe.input.keydown(Key.key_w);
+        return Luxe.input.keydown(_input.up);
     }
 
     public function playerDown(): Bool {
-        return Luxe.input.keydown(Key.key_s);
+        return Luxe.input.keydown(_input.down);
     }
 
     public function playerLeft(): Bool {
-        return Luxe.input.keydown(Key.key_a) || Luxe.input.keydown(Key.key_q);
+        return Luxe.input.keydown(_input.left);
     }
 
     public function playerRight(): Bool {
-        return Luxe.input.keydown(Key.key_d);
+        return Luxe.input.keydown(_input.right);
     }
 
     public function playerAction(): Bool {
-        return Luxe.input.keydown(Key.space);
+        return Luxe.input.keydown(_input.action);
     }
 
     public function exit(): Bool {
-        return Luxe.input.keydown(Key.escape);  
+        return Luxe.input.keydown(_input.exit);  
     }
 }
